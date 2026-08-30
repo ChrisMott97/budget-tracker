e794057be9d1ddc35f4b50139a3a6381ae5ef7dd
Chris Mott <ChrisMott97@users.noreply.github.com>
2026-08-30 20:32:46 +0100

    feat(app): add basic form and table to show parsed transaction result

--- app/src/pages/CsvImportPage.tsx
+++ app/src/pages/CsvImportPage.tsx
@@ -1,5 +1,67 @@
+import { useState } from "react";
+
+interface Transaction {
+    date: string;
+    description: string;
+    amount: number;
+}
+
 export default function CsvImportPage() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+    function submit(formData: FormData) {
+        const file = formData.get("csvFile") as File;
+        if (!file) {
+            throw new Error("No file selected");
+        }
+
+        const body = new FormData();
+        body.append("file", file);
+
+        fetch("/api/transactions", {
+            method: "POST",
+            body,
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed to import CSV");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setTransactions(data);
+            })
+            .catch((error) => {
+                console.error("Error importing CSV:", error);
+            });
+    }
+
+    const rows = transactions.map(transaction => (
+        <tr key={transaction.date + transaction.description}>
+            <td>{transaction.date}</td>
+            <td>{transaction.description}</td>
+            <td>£{transaction.amount.toFixed(2)}</td>
+        </tr>
+    ))
+
     return (
-        <div>Csv page</div>
+        <>
+            <form action={submit} className="flex flex-col gap-4 items-start">
+                <input type="file" name="csvFile" accept=".csv" />
+                <button type="submit">Import CSV</button>
+            </form>
+            <table className="table-fixed w-150">
+                <thead>
+                    <tr className="text-left">
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        </>
     );
 }
